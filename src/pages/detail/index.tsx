1d9a61d221a602ac742235376b202f3d1e2c6120
Maia-Mateus <matteusmaia889@gmail.com>
2023-08-22 11:58:30 -0300

    basic config

---
+++ src/pages/detail/index.tsx
@@ -0,0 +1,10 @@
+
+
+
+export function Detail(){
+    return(
+        <div>
+            <h1>Página home</h1>
+        </div>
+    )
+}
---
+++ src/pages/sobre/index.tsx
@@ -0,0 +1,10 @@
+
+
+
+export function Home(){
+    return(
+        <div>
+            <h1>Página home</h1>
+        </div>
+    )
+}
