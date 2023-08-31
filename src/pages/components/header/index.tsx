764fa36f9236d2773e92a7418159a51e18bfbfd7
Maia-Mateus <matteusmaia889@gmail.com>
2023-08-30 23:49:37 -0300

    .

--- src/pages/components/header/index.tsx
+++ src/pages/components/header/index.tsx
@@ -1,8 +1,16 @@
+import { Link } from 'react-router-dom'
+
+import styles from './header.module.css'
+import logoImg from '../../../assets/logo.svg'
 
 export function Header(){
     return(
-       <header>
-            <h1>Hedaer!</h1>
+       <header className={styles.container}>
+            <div>
+                <Link to='/'> 
+                    <img src={logoImg} alt="logo-cripto" />
+                </Link>
+            </div>
        </header>
     )
 }
--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,10 +1,51 @@
-
+import styles from './home.module.css'
+import { BiSearch } from 'react-icons/bi'
+import { Link } from 'react-router-dom'
 
 
 export function Home(){
     return(
-        <div>
-            <h1>Página home</h1>
-        </div>
+        <main className={styles.container}>
+            <form className={styles.form}>
+                <input 
+                    placeholder='Digite o simbolo da moeda: BTC...' 
+                />
+                <button type='submit'>
+                    <BiSearch size={30} color="#fff"/>
+                </button>
+            </form>
+
+            <table>
+                <thead>
+                    <tr>
+                        <th scope="col">Moeda</th>
+                        <th scope="col">Valor mercado</th>
+                        <th scope="col">Preço</th>
+                        <th scope="col">Volume</th>
+                    </tr>
+                </thead>
+
+                <tbody>
+                    <tr className={styles.tr}>
+                        <td className={styles.tdLabel} id='tbody'>
+                            <Link to='/detail/btc'>
+                                <span className={styles.link}>Bitcoin</span> | BTC
+                            </Link>
+                        </td>
+                        <td className={styles.tdLabel} >
+                            R$ 19293
+                        </td>
+                         <td className={styles.tdLabel} >
+                            R$ 40.321
+                        </td>
+                        <td className={styles.tdProfit} >
+                            <span>-5.3</span>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+        </main>
+
+
     )
 }
